e8a8da8ec07ec06f1bc092e127e6170a552fe665
ArunGovil <a.arungovil@gmail.com>
2022-12-06 17:17:27 +0530

    remove/search-header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import SecondarySearch from "./SecondarySearch";
 
-interface HeaderProps {
-  showSearch: boolean;
-}
+interface HeaderProps {}
 
-export default function Header({ showSearch }: HeaderProps) {
+export default function Header() {
   const logo = require("../public/img/cmdr.png");
   return (
     <nav className="flex justify-between p-4 items-center w-full max-w-5xl">
       <Link href="/">
         <Image src={logo} alt="cmdr" className="w-12" />
       </Link>
-      {showSearch && <SecondarySearch />}
       <a target="_blank" rel="noreferrer" href="https://github.com/ArunGovil">
         <p className="font-medium text-sm hover:text-orange-400">@github</p>
       </a>
